d156f04cd3e86b53a7e0b6751ebc157dd315ea11
antklim <antklim@gmail.com>
2020-08-25 20:40:48 +1000

    Go module inited

--- enum/enum_test.go
+++ enum/enum_test.go
@@ -1,1 +1,71 @@
 package enum_test
+
+import "testing"
+
+func TestUser_MarshallJSON(t *testing.T) {
+	t.Skip("should marshall User structure to JSON")
+
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
+
+func TestUser_UnmarshallJSON(t *testing.T) {
+	t.Skip("should unmarshall JSON to User structure")
+
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
+
+func TestUser_MarshallYAML(t *testing.T) {
+	t.Skip("should marshall User structure to YAML")
+
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
+
+func TestUser_UnmarshallYAML(t *testing.T) {
+	t.Skip("should unmarshall YAML to User structure")
+
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
