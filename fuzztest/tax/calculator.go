1446387b3cf19a5c72ce228cd38a28d5eb6ff07f
antklim <antklim@gmail.com>
2020-09-22 13:37:47 +1000

    Tax calculator example inited

---
+++ fuzztest/tax/calculator.go
@@ -0,0 +1,19 @@
+package tax
+
+// Calc2018 tax calculator for 2018-2019 financial year.
+func Calc2018(p Payer) float64 {
+	// TODO: implement
+	return 0.0
+}
+
+// Calc2019 tax calculator for 2019-2020 financial year.
+func Calc2019(p Payer) float64 {
+	// TODO: implement
+	return 0.0
+}
+
+// Calc2020 tax calculator for 2019-2020 financial year.
+func Calc2020(p Payer) float64 {
+	// TODO: implement
+	return 0.0
+}
--- fuzztest/tax/tax.go
+++ fuzztest/tax/tax.go
@@ -1,1 +1,26 @@
 package tax
+
+// Tax agency provides tax strategy
+// Tax strategies differ from year to year
+// Tax calculator takes year and income and returns tax information
+
+// Period represents tax period.
+type Period string
+
+// Income represents payer income for tax period.
+type Income map[Period]float64
+
+// Payer represents tax payer.
+type Payer struct {
+	tfn    string
+	income Income
+}
+
+// PeriodCalculator represents tax periods and related calculators.
+type PeriodCalculator map[Period]Calculator
+
+// Calculator returns the tax value for the payer in the local currency.
+type Calculator func(p Payer) float64
+
+// Agency represents tax agency that collects taxes.
+type Agency string
